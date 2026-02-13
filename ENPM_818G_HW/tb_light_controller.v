087c6f6e6cc013711ad31091bbed90de7676ac2b
Vinay Agarwal <agarvini@protonmail.com>
2026-02-13 12:33:05 -0500

    Update to the HW1 light contoller

--- ENPM_818G_HW/tb_light_controller.v
+++ ENPM_818G_HW/tb_light_controller.v
@@ -29,6 +29,17 @@ module tb_light_controller ();
         .car_has_arrived(car_has_arrived),
         .current_light_state(current_light_state),
         .street_light_controller(street_light_controller),
-        .set_light_color(set_light_color));
+        .set_light_color(set_light_color)
+    );
 
+    initial begin 
+        $monitor("clk=%d, car_has_arrived=%d", clk, car_has_arrived);
+    end
+
+    initial begin
+        #1; // wait 1 time unit
+
+    end
+    
+endmodule
     
