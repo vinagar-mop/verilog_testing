c0b2b0f386e70913409feaa7bc3fca0b03dce480
Vinay Agarwal <agarvini@protonmail.com>
2026-02-15 12:32:14 -0500

    Changing light controller

--- ENPM_818G_HW/light_controller.v
+++ ENPM_818G_HW/light_controller.v
@@ -1,5 +1,11 @@
 module light_controller(
 
+    // The set state of the light currently
+    // 0 = red
+    // 3 = green
+    // 1 = yellow
+    output reg [1:0] set_light_color,
+
     // Clock input
     // Clk
     input clk,
@@ -16,23 +22,11 @@ module light_controller(
     // Street Light State
     // 1 can allow green
     // 0 can't allow green
-    input street_light_controller,
-
-    // The set state of the light currently
-    // 0 = red
-    // 3 = green
-    // 1 = yellow
-    output reg [1:0] set_light_color);
+    input street_light_controller);
 
     reg [3:0] yield_time = 0;
     reg [8:0] go_time = 0;
 
-    always @(set_light_color) begin
-        $monitor("\n\t MON current_light_state = %d,\n street_light_controller = %d,\n set_light_color = %d\n",
-                current_light_state,
-                street_light_controller,
-                set_light_color);
-    end
 
     // keep track GREEN
     // Transitioning to YIELD
@@ -57,13 +51,10 @@ module light_controller(
                 assign set_light_color = 2'b00;    // setting the lights to RED
             end
         end
-    end
-
-    // Keeping track of RED
-    // Transitioning to GREEN
-    initial begin
-        if (street_light_controller) begin
-            assign set_light_color = 2'b11; // Setting light to GREEN
+        else if (current_light_state == 2'b00) begin 
+            if (street_light_controller) begin
+                assign set_light_color = 2'b11; // Setting light to GREEN
+            end
         end
     end
 
